cff4139b396c8b975ad62d1a0e56d886c726ebb6
olakayCoder1 <programmerolakay@gmail.com>
2024-11-27 21:58:15 +0100

    Modified app run configuration in app.py

--- app.py
+++ app.py
@@ -4,5 +4,6 @@ app , celery = create_app()
 app.app_context().push()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
     # app.run(port=8000)
