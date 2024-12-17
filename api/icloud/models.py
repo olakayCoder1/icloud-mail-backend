e1e51936acbfdeb3c661f6950a9c819b7a5cd3a9
olakayCoder1 <programmerolakay@gmail.com>
2024-12-17 02:26:00 +0100

    "Added a duplicate comment to the __tablename__ attribute in the Account model."

--- api/icloud/models.py
+++ api/icloud/models.py
@@ -3,7 +3,7 @@ from ..helpers.utils import db
 from sqlalchemy.dialects.postgresql import UUID
 
 class Account(db.Model):
-    __tablename__ = 'accounts'
+    __tablename__ = 'accounts' 
     
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)  # Use UUID as primary key
     email = db.Column(db.String(120), unique=True, nullable=False)
